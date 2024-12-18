d0395720b7863ec0c94ddc548bd24b6b3239179c
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-12-19 03:24:03 +0800

    path

--- cuda-SIFT/run.sh
+++ cuda-SIFT/run.sh
@@ -1,5 +1,5 @@
-./my_app /workspaces/Frank/Parallel/Project/cuda/data/512x640.jpg;
+./my_app data/512x640.jpg;
 sleep 2;
-./my_app /workspaces/Frank/Parallel/Project/cuda/data/2092x2656.jpg;
+./my_app data/2092x2656.jpg;
 sleep 2;
-./my_app /workspaces/Frank/Parallel/Project/cuda/data/3989x5983_1.jpg;
+./my_app data/3989x5983_1.jpg;
