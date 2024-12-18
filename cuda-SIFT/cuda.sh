a802363ebf0a55ed9ad42cc23daffa268f6c3f4f
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-12-19 03:21:15 +0800

    t

--- cuda-SIFT/cuda.sh
+++ cuda-SIFT/cuda.sh
@@ -1,2 +1,2 @@
 # CUDA
-nvcc -o my_app -gencode=arch=compute_86,code=sm_86 -Xcompiler '-fPIC' -g -O3 find_keypoints.cpp image.cu sift.cpp
+nvcc -o my_app -gencode=arch=compute_86,code=sm_86 -Xcompiler '-fPIC' -g -O3 find_keypoints.cpp image.cu sift.cpp -diag-suppress 550-D
